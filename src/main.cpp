d6ab719000058707a26a0889ae9249952b6086b9
Christian Mayr <RyamBaCo@gmail.com>
2013-04-28 12:22:38 +0200

    created empty open frameworks project

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include "pizzaApp.h"
+#include "ofAppGlutWindow.h"
+
+int main()
+{
+	ofAppGlutWindow window; // create a window
+	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
+	ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);
+	ofRunApp(new pizzaApp()); // start the app
+}
---
+++ src/pizzaApp.cpp
@@ -0,0 +1,49 @@
+#include "pizzaApp.h"
+
+void pizzaApp::setup()
+{
+}
+
+void pizzaApp::update()
+{
+}
+
+void pizzaApp::draw()
+{
+}
+
+void pizzaApp::keyPressed(int key)
+{
+}
+
+void pizzaApp::keyReleased(int key)
+{
+}
+
+void pizzaApp::mouseMoved(int x, int y)
+{
+}
+
+void pizzaApp::mouseDragged(int x, int y, int button)
+{
+}
+
+void pizzaApp::mousePressed(int x, int y, int button)
+{
+}
+
+void pizzaApp::mouseReleased(int x, int y, int button)
+{
+}
+
+void pizzaApp::windowResized(int w, int h)
+{
+}
+
+void pizzaApp::gotMessage(ofMessage msg)
+{
+}
+
+void pizzaApp::dragEvent(ofDragInfo dragInfo)
+{
+}
